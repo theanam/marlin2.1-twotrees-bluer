ee38ff61d39872d200ed0d8a89f8cfeb5dbd0776
Anam <90724295+anam-zelf@users.noreply.github.com>
2022-12-04 13:27:24 +0600

    z height fix

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1478,7 +1478,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, -36, -2.7 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, -36, -2.5 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
