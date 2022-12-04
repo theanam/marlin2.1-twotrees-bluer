a579e7fe9b414664eca1fa7343d8cf89d73f706e
Anam <90724295+anam-zelf@users.noreply.github.com>
2022-12-04 12:39:01 +0600

    updated nozzle offset values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1478,11 +1478,11 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 38, -7, -2.8 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, -36, -2.7 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 20
+#define PROBING_MARGIN 37
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_FEEDRATE (133*60)
