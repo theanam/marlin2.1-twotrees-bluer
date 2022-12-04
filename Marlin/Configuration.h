57f6557df749a2ffb3035b453bb777392b2b5d6e
Anam <90724295+anam-zelf@users.noreply.github.com>
2022-12-04 12:25:02 +0600

    new probe holder support

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1480,11 +1480,11 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 38, -7, -1.6 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, -36, -1.6 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 20
+#define PROBING_MARGIN 30
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_FEEDRATE (133*60)
